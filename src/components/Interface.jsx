3a4623fa4fa5ecfbc775e501e9e4cb424dd228fe
justin etienne <juetienne7@gmail.com>
2024-06-04 22:19:00 +0900

    font changed + link address added

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -11,26 +11,26 @@ function Interface() {
         <div className="mt-2">
           <p className="text-lg">Here are all my Links.<br /> Feel free to check them out!</p>
           <ul className="w-72 sm:w-96 m-auto mt-2">
-            <li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200">
+            <a href="mailto:juetienne7@gmail.com"><li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200">
               <img loading="eager" src="./icons/gmail.png" alt="" />
               <p>juetienne7@gmail.com</p>
-            </li>
-            <li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
+            </li></a>
+            <a href="https://www.linkedin.com/in/justin-etienne" target="_blank"><li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
               <img loading="eager" src="./icons/linkedin.png" alt="" />
               Linkedin
-            </li>
-            <li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
+            </li></a>
+            <a href="https://www.wantedly.com/id/justin_etienne" target="_blank"><li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
               <img loading="eager" src="./icons/wantedly.png" alt="" />
               Wantedly
-            </li>
-            <li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
+            </li></a>
+            <a href="https://github.com/Jasufr" target="_blank"><li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
               <img loading="eager" src="./icons/github.png" alt="" />
               Github
-            </li>
-            <li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
+            </li></a>
+            <a href="https://www.justinetienne.com/" target="_blank"><li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
               <img loading="eager" src="./icons/portfolio.png" alt="" />
               Portfolio
-            </li>
+            </li></a>
           </ul>
         </div>
         <div className="absolute bottom-2 left-1/2 -translate-x-1/2 w-full text-sm">
