5772bce11ac84581949558af3d9e48cfcf4dfe80
justin etienne <juetienne7@gmail.com>
2024-06-04 21:39:00 +0900

    sun position changing with time

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,18 +1,57 @@
 import { Environment, Sky } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useThree } from "@react-three/fiber";
+import { useEffect, useState } from "react";
 
 function Experience() {
 
-  const {viewport} = useThree();
+  const { viewport } = useThree();
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [sunPosition, setSunPosition] = useState([0, 0, 0]);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(timerId);
+  }, []);
+
+  useEffect(() => {
+    const hours = currentTime.getHours();
+    let newSunPosition;
+
+    if (hours >= 0 && hours < 4) {
+      newSunPosition = [10, -1, 10];
+    } else if (hours >= 4 && hours < 6) {
+      newSunPosition = [10, 0, 10];
+    } else if (hours >= 6 && hours < 8) {
+      newSunPosition = [10, 1, 10];
+    } else if (hours >= 8 && hours < 10) {
+      newSunPosition = [10, 2, 10];
+    } else if (hours >= 10 && hours < 15) {
+      newSunPosition = [10, 3, 10];
+    } else if (hours >= 15 && hours < 17) {
+      newSunPosition = [10, 2, 10];
+    } else if (hours >= 17 && hours < 18) {
+      newSunPosition = [10, 1, 10];
+    } else if (hours >= 18 && hours < 21) {
+      newSunPosition = [10, 0, 10];
+    } else if (hours >= 21 && hours <= 24) {
+      newSunPosition = [10, -1, 10];
+    }
+
+    setSunPosition(newSunPosition);
+  }, [currentTime]);
   return (
     <>
       <Environment preset="sunset" />
-      <Sky sunPosition={[10, -1, 10]} />
+      <Sky sunPosition={sunPosition} />
       <Avatar
-          position-y={-1}
-          animation={"Falling"}
-        />
+        position-y={-1}
+        animation={"Falling"}
+      />
     </>
   );
 }
--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,4 +1,5 @@
 function Interface() {
+
   return (
     <>
       <div className="container m-auto text-white text-center">
@@ -11,23 +12,23 @@ function Interface() {
           <p className="text-lg">Here are all my Links.<br /> Feel free to check them out!</p>
           <ul className="w-72 sm:w-96 m-auto mt-2">
             <li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200">
-              <img src="./icons/gmail.png" alt="" />
+              <img loading="eager" src="./icons/gmail.png" alt="" />
               <p>juetienne7@gmail.com</p>
             </li>
             <li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
-              <img src="./icons/linkedin.png" alt="" />
+              <img loading="eager" src="./icons/linkedin.png" alt="" />
               Linkedin
             </li>
             <li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
-              <img src="./icons/wantedly.png" alt="" />
+              <img loading="eager" src="./icons/wantedly.png" alt="" />
               Wantedly
             </li>
             <li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
-              <img src="./icons/github.png" alt="" />
+              <img loading="eager" src="./icons/github.png" alt="" />
               Github
             </li>
             <li className="flex items-center gap-3 bg-white bg-opacity-20 py-2 px-4 rounded-sm hover:bg-opacity-50 transition-all duration-200 mt-3 shadow-md">
-              <img src="./icons/portfolio.png" alt="" />
+              <img loading="eager" src="./icons/portfolio.png" alt="" />
               Portfolio
             </li>
           </ul>
